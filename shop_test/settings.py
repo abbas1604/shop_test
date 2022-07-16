d93f0cf44fb9bab45e7ebd22ade0cbd6d90c1464
The killer of global Zionism <U@gmail.com>
2022-07-16 14:03:56 +0430

    connecting to mysql

--- shop_test/settings.py
+++ shop_test/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'shop_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '*************',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '*********'
     }
 }
 
