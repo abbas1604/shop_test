2f9b429bfe0c3ec72fa8f283894e410cab39a504
The killer of global Zionism <U@gmail.com>
2022-07-16 14:11:49 +0430

    add apps to settings.py/INSTALLED_APPS

--- shop_test/settings.py
+++ shop_test/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'store',
+    'tags',
+    'likes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
