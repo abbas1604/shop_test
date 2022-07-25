fea90a806341abad07e1eaedc88a4c4c49e5450a
The killer of global Zionism <U@gmail.com>
2022-07-25 08:32:41 +0430

    adding computed column

--- store/admin.py
+++ store/admin.py
@@ -5,9 +5,15 @@ from store import models
 
 @admin.register(models.Product)
 class ProductModel(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'LOW'
+        return 'OK'
 
 
 @admin.register(models.Customer)
