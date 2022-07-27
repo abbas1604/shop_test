e5738f7c2fa8f82a03fdba65c576f718fc39be39
The killer of global Zionism <U@gmail.com>
2022-07-27 10:18:37 +0430

    providing links to other pages

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 
 from store import models
 
@@ -32,4 +35,20 @@ class OrderModel(admin.ModelAdmin):
     list_display = ['id', 'placed_at', 'customer']
 
 
-admin.site.register(models.Collection)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = (reverse('admin:store_product_changelist')
+               + '?'
+               + urlencode({
+                    'collection__id': str(collection.id)
+                }))
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
