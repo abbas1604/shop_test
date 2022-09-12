c0d2100288301b0791639a0f1de90f118ff72a4b
The killer of global Zionism <U@gmail.com>
2022-09-12 10:01:24 +0430

    exception handler

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -7,6 +8,6 @@ from store.serializers import ProductSerializer
 
 @api_view()
 def product_detail(request, id):
-    product = Product.objects.get(pk=id)
+    product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
